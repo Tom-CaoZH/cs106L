b0e308cf0a050b84563a315c6602629d00b83e11
tom cao <2359991846@qq.com>
2022-05-22 22:41:05 +0800

    simple all add const and success

--- hws/HashMap_Starter/hashmap.cpp
+++ hws/HashMap_Starter/hashmap.cpp
@@ -21,17 +21,17 @@ HashMap<K, M, H>::~HashMap() {
 }
 
 template <typename K, typename M, typename H>
-inline size_t HashMap<K, M, H>::size() {
+inline size_t HashMap<K, M, H>::size() const{
     return _size;
 }
 
 template <typename K, typename M, typename H>
-inline bool HashMap<K, M, H>::empty() {
+inline bool HashMap<K, M, H>::empty() const{
     return size() == 0;
 }
 
 template <typename K, typename M, typename H>
-inline float HashMap<K, M, H>::load_factor() {
+inline float HashMap<K, M, H>::load_factor() const{
     return static_cast<float>(size())/bucket_count();
 };
 
@@ -41,7 +41,7 @@ inline size_t HashMap<K, M, H>::bucket_count() const{
 };
 
 template <typename K, typename M, typename H>
-M& HashMap<K, M, H>::at(const K& key) {
+M& HashMap<K, M, H>::at(const K& key) const{
     auto [prev, node_found] = find_node(key);
             if (node_found == nullptr) {
         throw std::out_of_range("HashMap<K, M, H>::at: key not found");
@@ -50,7 +50,7 @@ M& HashMap<K, M, H>::at(const K& key) {
 }
 
 template <typename K, typename M, typename H>
-bool HashMap<K, M, H>::contains(const K& key) {
+bool HashMap<K, M, H>::contains(const K& key) const{
     return find_node(key).second != nullptr;
 }
 
@@ -65,7 +65,7 @@ void HashMap<K, M, H>::clear() {
 }
 
 template <typename K, typename M, typename H>
-typename HashMap<K, M, H>::iterator HashMap<K, M, H>::find(const K& key) {
+typename HashMap<K, M, H>::iterator HashMap<K, M, H>::find(const K& key) const{
     return make_iterator(find_node(key).second);
 }
 
@@ -164,7 +164,7 @@ typename HashMap<K, M, H>::iterator HashMap<K, M, H>::erase(typename HashMap<K, 
 }
 
 template <typename K, typename M, typename H>
-    void HashMap<K, M, H>::debug() {
+    void HashMap<K, M, H>::debug() const{
     std::cout << std::setw(30) << std::setfill('-') << '\n' << std::setfill(' ')
           << "Printing debug information for your HashMap implementation\n"
           << "Size: " << size() << std::setw(15) << std::right
@@ -221,7 +221,7 @@ HashMap<K, M, H>::HashMap(std::initializer_list<value_type> init, size_t bucket_
 }
 
 template <typename K, typename M, typename H>
-M& HashMap<K, M, H>::operator[](const K& key) {
+M& HashMap<K, M, H>::operator[](const K& key) const{
    return insert({key, {}}).first->second;
 }
 
